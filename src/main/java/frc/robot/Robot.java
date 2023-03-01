8bdadbda09c4dfcce6abe534ec5876367921fe71
JaimeHub <126204063+JaimeHub@users.noreply.github.com>
2023-02-28 22:38:59 -0600

    Add files via upload

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,9 @@
+<xml version="1.0">
+<dashboard>
+	<widget field="encoder value" type="Number" class="edu.wpi.first.smartdashboard.gui.elements.TextBox">
+		<location x="5" y="11"/>
+	</widget>
+</dashboard>
+<live-window>
+</live-window>
+</xml>
